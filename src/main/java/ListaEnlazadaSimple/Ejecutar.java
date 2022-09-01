06b101fc606e817f6f932ce4e9b1fa8d16851698
Bryan <bramirezo2@miumg.edu.gt>
2022-08-31 23:00:05 -0600

    them.

---
+++ src/main/java/ListaEnlazadaSimple/Ejecutar.java
@@ -0,0 +1,21 @@
+package ListaEnlazadaSimple;
+
+public class Ejecutar {
+    public static void main (String[] args){
+     Lista l = new Lista();
+        l.insertarNodo();
+        l.insertarNodo();
+        l.insertarNodo();
+        l.insertarNodo();
+        l.insertarNodo();
+
+        System.out.println("Los Nodos Ingresados son");
+        l.desplegarLista();
+        l.eliminarNodo();
+        l.desplegarLista();
+    }
+
+
+
+
+}
